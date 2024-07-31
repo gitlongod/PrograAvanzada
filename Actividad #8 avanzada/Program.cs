69005e7053a5f5d074fc3c6040ec07aff0525ef0
gto26 <nxsmaritogtohernandez@gmail.com>
2024-07-31 09:26:42 -0600

    Creación de funcion para menu y para switch y creación de métodos para agregar receta y buscar receta

--- Actividad #8 avanzada/Program.cs
+++ Actividad #8 avanzada/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using Actividad__8_avanzada;
+
+bool right = true;
+List<Recetas> recetasList = new List<Recetas>();
+int idPosition = 0;
+int quantity = 0;
+do
+{
+	try
+	{
+
+	}
+	catch (Exception)
+	{
+
+		throw;
+	}
+} while (right);
+
+static int Menu()
+{
+    Console.WriteLine("Escoja una opción");
+    Console.WriteLine("1.Agregar");
+    Console.WriteLine("2.Buscar");
+    Console.WriteLine("3.Mostrar");
+    Console.WriteLine("4.Salir");
+    int option = int.Parse(Console.ReadLine());
+	return option;
+}
+static void ShowSiwtch()
+{
+	switch(Menu())
+	{
+		case 1:
+			break;
+			case 2:
+			break;
+			case 3:
+			break;
+			case 4:
+			break;
+	}
+}
---
+++ Actividad #8 avanzada/Recetas.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad__8_avanzada
+{
+    internal class Recetas
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string  Ingridients { get; set; }
+        public string  Process { get; set; }
+
+        public Recetas(string iD, string name, string ingridients, string process)
+        {
+            ID = iD;
+            Name = name;
+            Ingridients = ingridients;
+            Process = process;
+        }
+        public static void AddRecipe(ref int quantity, ref List<Recetas> recetasList)
+        {
+            Console.WriteLine("Ingrese el ID de la receta");
+            string idRecipe = Console.ReadLine();   
+            Console.WriteLine("Ingrese el nombre de la receta");
+            string nameRecipe = Console.ReadLine();
+            Console.WriteLine("Ingrese los ingredientes de la lista");
+            string ingridientRecipe = Console.ReadLine();
+            Console.WriteLine("Ingrese el procedimiento de la receta");
+            string processRecipe = Console.ReadLine();
+            Recetas newRecipe = new Recetas(idRecipe,nameRecipe,ingridientRecipe,processRecipe);
+            recetasList.Add(newRecipe);
+            Console.WriteLine("La receta se ha agregado exitosamente");
+            Console.ReadKey();
+        }
+        public static void FindRecipe(ref List<Recetas> recetasList)
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el ID de la receta que desea buscar");
+            string idRecipe = Console.ReadLine();
+            Recetas findRecipe = recetasList.Find(p => p.ID == idRecipe);
+            if (findRecipe != null) 
+            {
+                Console.WriteLine("La receta ya existe");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("La receta aun no existe");
+            }
+        }
+    }
+}
